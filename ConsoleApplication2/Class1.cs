67bdcb5bbd0dc19d7ba57433aa6058961ba5ef4f
Cieciol <tomasz.szczygielski@pollub.edu.pl>
2016-05-15 17:53:16 +0100

    lets start working with git
    just various code to get basics of c#. Last time i added a class diagram
    and a my first class. hello object oriented programming

---
+++ ConsoleApplication2/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Employee 
+    {
+        private string empName;
+        private int empID;
+        private float currPay;
+
+        public Employee() { };
+        public Employee(string name, int ID, float pay )
+        {
+            
+        }
+
+
+        public string name
+        {
+            get
+            {
+                return empName;
+            }
+
+            set
+            {
+                empName = value;
+            }
+
+        }
+        public int ID
+        {
+            get
+            {
+                return empID;
+            }
+
+            set
+            {
+                empID = value;
+            }
+        } 
+        public float Pay
+        {
+            get
+            {
+                return currPay;
+            }
+            set
+            {
+                currPay = value; 
+            }
+        }
+
+}
